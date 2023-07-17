4ea2b3124f57f5f5e438280d04d00e2d822d32cb
mmmihhaeel <mikael11032005@gmail.com>
2023-07-17 10:46:32 +0300

    finished

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const app = express();
 
 const pagesRoutes = require('./routes/pages');
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(pagesRoutes);
@@ -19,7 +15,7 @@ app.use(pagesRoutes);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Process ${process.pid}, URL http://localhost:${port}, Server is being listened on ${port}`);
+  console.log(`Process ${process.pid}, URL http://localhost:${port}, Server is being listened on ${port}`);
 });
 
 module.exports = app;
--- routes/pages.js
+++ routes/pages.js
@@ -6,6 +6,10 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.sendFile(path.join(rootDir, 'views', 'index.html'));
+});
+
 router.get('/first-project', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'first-project.html'));
 });
@@ -16,6 +20,38 @@ router.get('/second-project', (req, res, next) => {
 
 router.get('/third-project', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'third-project.html'));
+});
+
+router.get('/bubbles', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'bubbles.html'));
+});
+
+router.get('/liquid', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'liquid.html'));
+});
+
+router.get('/constellations_yellow', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'constellations_yellow.html'));
+});
+
+router.get('/rainbow', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'rainbow.html'));
+});
+
+router.get('/sunrays', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'sunrays.html'));
+});
+
+router.get('/disapearing-lines', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'disapearing-lines.html'));
+});
+
+router.get('/constellations_outlines', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'constellations_outlines.html'));
+});
+
+router.get('/color_split', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'color_split.html'));
 });
 
 module.exports = router;
